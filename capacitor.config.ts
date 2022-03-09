7e61cee8c83aae95d5895a6481ae4299d3982126
LittleSheep <littlesheep@LittleSheepdeMacBook-Pro.local>
2022-03-09 22:26:16 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'EatingSafety',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
